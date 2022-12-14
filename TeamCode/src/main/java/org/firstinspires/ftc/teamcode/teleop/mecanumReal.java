29dc97a683575d05229ba33625772982ee354f45
Sam Freund <techguy763@gmail.com>
2022-12-14 15:04:00 +0000

    switch auto to manual

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/mecanumReal.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/mecanumReal.java
@@ -39,7 +39,8 @@ import com.qualcomm.robotcore.hardware.DcMotorController;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
-
+import java.lang.reflect.Parameter;
+import java.util.Locale;
 import org.firstinspires.ftc.robotcore.external.navigation.Acceleration;
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
@@ -47,176 +48,177 @@ import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
 import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
 import org.firstinspires.ftc.teamcode.PIDController;
 
-import java.lang.reflect.Parameter;
-import java.util.Locale;
-
 @TeleOp(name = "mecanumReal", group = "Competition")
 // @Disabled
 public class mecanumReal extends LinearOpMode {
 
-    public static Orientation angles;
-    public static Acceleration gravity;
-    public static final int[] slidePosArray = {0, 33, 50, 100};
+  public static Orientation angles;
+  public static Acceleration gravity;
+  public static final int[] slidePosArray = { 0, 33, 50, 100 };
 
-    private int manualOrAuto = 0;
+  private boolean autoSlides = true;
 
-    //button press
-    private boolean isX = false;
-    private boolean isY = false;
-    private boolean isA = false;
-    private boolean isB = false;
+  //button press
+  private boolean isX = false;
+  private boolean isY = false;
+  private boolean isA = false;
+  private boolean isB = false;
 
-    //button press stuff
-    private boolean wasX = false;
-    private boolean wasY = false;
-    private boolean wasA = false;
-    private boolean wasB = false;
+  //button press stuff
+  private boolean wasX = false;
+  private boolean wasY = false;
+  private boolean wasA = false;
+  private boolean wasB = false;
 
-    BNO055IMU imu = hardwareMap.get(BNO055IMU.class, "imu");
-    BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+  BNO055IMU imu = hardwareMap.get(BNO055IMU.class, "imu");
+  BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
 
+  @Override
+  public void runOpMode() throws InterruptedException {
+    // Declare OpMode members.
+    DcMotor lF = hardwareMap.dcMotor.get("front_left");
+    DcMotor lB = hardwareMap.dcMotor.get("back_left");
+    DcMotor rF = hardwareMap.dcMotor.get("front_right");
+    DcMotor rB = hardwareMap.dcMotor.get("back_right");
+    DcMotor slideOne = hardwareMap.dcMotor.get("slide_one");
+    DcMotor slideTwo = hardwareMap.dcMotor.get("slide_two");
 
-    @Override
-    public void runOpMode() throws InterruptedException {
+    imu.initialize(parameters);
 
-        // Declare OpMode members.
-        DcMotor lF = hardwareMap.dcMotor.get("front_left");
-        DcMotor lB = hardwareMap.dcMotor.get("back_left");
-        DcMotor rF = hardwareMap.dcMotor.get("front_right");
-        DcMotor rB = hardwareMap.dcMotor.get("back_right");
-        DcMotor slideOne = hardwareMap.dcMotor.get("slide_one");
-        DcMotor slideTwo = hardwareMap.dcMotor.get("slide_two");
+    telemetry.addData("Status", "Initialized");
+    telemetry.update();
 
-        imu.initialize(parameters);
+    // Set motor directions
+    lF.setDirection(DcMotor.Direction.FORWARD);
+    rF.setDirection(DcMotor.Direction.REVERSE);
+    lB.setDirection(DcMotor.Direction.FORWARD);
+    rB.setDirection(DcMotor.Direction.REVERSE);
+    slideOne.setDirection(DcMotor.Direction.REVERSE);
+    slideTwo.setDirection(DcMotorSimple.Direction.REVERSE);
 
-        telemetry.addData("Status", "Initialized");
-        telemetry.update();
+    // Set zero power behavior
+    lF.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+    rF.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+    lB.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+    rB.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+    slideOne.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+    slideTwo.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
-        // Set motor directions
-        lF.setDirection(DcMotor.Direction.FORWARD);
-        rF.setDirection(DcMotor.Direction.REVERSE);
-        lB.setDirection(DcMotor.Direction.FORWARD);
-        rB.setDirection(DcMotor.Direction.REVERSE);
-        slideOne.setDirection(DcMotor.Direction.REVERSE);
-        slideTwo.setDirection(DcMotorSimple.Direction.REVERSE);
+    //set encoder behavior
+    slideOne.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+    slideTwo.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
-        // Set zero power behavior
-        lF.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        rF.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        lB.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        rB.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        slideOne.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        slideTwo.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+    //variables
+    int slidePos = 0;
 
-        //set encoder behavior
-        slideOne.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        slideTwo.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+    PIDController slideOneController = new PIDController(0.05, 0, 0, false);
+    PIDController slideTwoController = new PIDController(0.05, 0, 0, false);
+    int targetPosition = 0;
 
+    // Wait for the game to start (driver presses PLAY)
+    waitForStart();
 
-        //variables
-        int slidePos = 0;
+    // run until the end of the match (driver presses STOP)
+    while (opModeIsActive()) {
+      if (gamepad1.dpad_left) {
+        autoSlides = false;
+      } else if (gamepad1.dpad_right) {
+        autoSlides = true;
+      }
 
-        PIDController slideOneController = new PIDController(0.05, 0, 0, false);
-        PIDController slideTwoController = new PIDController(0.05, 0, 0, false);
-        int targetPosition = 0;
-
-        // Wait for the game to start (driver presses PLAY)
-        waitForStart();
-
-        // run until the end of the match (driver presses STOP)
-        while (opModeIsActive()) {
-
-            //ground
-            if(manualOrAuto % 2 == 0) {
-                if ((isA = gamepad1.a) && !wasA) {
-                    slidePos = 0;
-                }
-                //high
-                else if ((isB = gamepad1.b) && !wasB) {
-                    slidePos = 3;
-                }
-                //medium
-                else if ((isX = gamepad1.x) && !wasX) {
-                    slidePos = 2;
-                }
-                //low
-                else if ((isY = gamepad1.y) && !wasY) {
-                    slidePos = 1;
-                }
-                targetPosition = slidePosArray[slidePos];
-            } else{
-                if(gamepad1.dpad_up){
-                    targetPosition++;
-                } else if(gamepad1.dpad_down){
-                    targetPosition--;
-                }
-            }
-            //sets target position to determined state
-
-
-            // call "update" method and prepare motorPower
-            double slideOnePower = slideOneController.update(targetPosition, slideOne.getCurrentPosition());
-            double slideTwoPower = slideTwoController.update(targetPosition, slideTwo.getCurrentPosition());
-
-            // assign motor the PID output
-            slideOne.setPower(slideOnePower);
-            slideTwo.setPower(slideTwoPower);
-
-            // Setup a variable for each drive wheel to save power level for telemetry
-            double frontLeftPower;
-            double frontRightPower;
-            double backLeftPower;
-            double backRightPower;
-
-            // set gamepad values
-            double x = gamepad1.left_stick_x * 1.1;
-            double y = -gamepad1.left_stick_y;
-            double t = gamepad1.right_stick_x;
-
-            // rotation
-            double x_rotated = x * Math.cos(angles.firstAngle) - y * Math.sin(angles.firstAngle);
-            double y_rotated = x * Math.sin(angles.firstAngle) + y * Math.cos(angles.firstAngle);
-
-            // x, y, theta input mixing
-            double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(t), 1);
-            frontLeftPower = (y + x + t) / denominator;
-            backLeftPower = (y - x + t) / denominator;
-            frontRightPower = (y - x - t) / denominator;
-            backRightPower = (y + x - t) / denominator;
-
-            // Send calculated power to motors
-            if (gamepad1.right_bumper) {
-                lF.setPower(frontLeftPower * 0.75);
-                rF.setPower(frontRightPower * 0.75);
-                lB.setPower(backLeftPower * 0.75);
-                rB.setPower(backRightPower * 0.75);
-
-            } else if (gamepad1.left_bumper) {
-                lF.setPower(frontLeftPower * 0.25);
-                rF.setPower(frontRightPower * 0.25);
-                lB.setPower(backLeftPower * 0.25);
-                rB.setPower(backRightPower * 0.25);
-
-            } else {
-                lF.setPower(frontLeftPower);
-                rF.setPower(frontRightPower);
-                lB.setPower(backLeftPower);
-                rB.setPower(backRightPower);
-            }
-
-
-            // reinitialize field oriented
-            if (gamepad1.left_trigger == 1 && gamepad1.right_trigger == 1) {
-                imu.initialize(parameters);
-            }
-
-            //one button press does one thing
-            wasA = isA;
-            wasX = isX;
-            wasY = isY;
-            wasB = isB;
+      //ground
+      if (autoSlides) {
+        if ((isA = gamepad1.a) && !wasA) {
+          slidePos = 0;
         }
+        //high
+        else if ((isB = gamepad1.b) && !wasB) {
+          slidePos = 3;
+        }
+        //medium
+        else if ((isX = gamepad1.x) && !wasX) {
+          slidePos = 2;
+        }
+        //low
+        else if ((isY = gamepad1.y) && !wasY) {
+          slidePos = 1;
+        }
+        targetPosition = slidePosArray[slidePos];
+      } else {
+        if (gamepad1.dpad_up) {
+          targetPosition++;
+        } else if (gamepad1.dpad_down) {
+          targetPosition--;
+        }
+      }
+      //sets target position to determined state
 
+      // call "update" method and prepare motorPower
+      double slideOnePower = slideOneController.update(
+        targetPosition,
+        slideOne.getCurrentPosition()
+      );
+      double slideTwoPower = slideTwoController.update(
+        targetPosition,
+        slideTwo.getCurrentPosition()
+      );
+
+      // assign motor the PID output
+      slideOne.setPower(slideOnePower);
+      slideTwo.setPower(slideTwoPower);
+
+      // Setup a variable for each drive wheel to save power level for telemetry
+      double frontLeftPower;
+      double frontRightPower;
+      double backLeftPower;
+      double backRightPower;
+
+      // set gamepad values
+      double x = gamepad1.left_stick_x * 1.1;
+      double y = -gamepad1.left_stick_y;
+      double t = gamepad1.right_stick_x;
+
+      // rotation
+      double x_rotated =
+        x * Math.cos(angles.firstAngle) - y * Math.sin(angles.firstAngle);
+      double y_rotated =
+        x * Math.sin(angles.firstAngle) + y * Math.cos(angles.firstAngle);
+
+      // x, y, theta input mixing
+      double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(t), 1);
+      frontLeftPower = (y + x + t) / denominator;
+      backLeftPower = (y - x + t) / denominator;
+      frontRightPower = (y - x - t) / denominator;
+      backRightPower = (y + x - t) / denominator;
+
+      // Send calculated power to motors
+      if (gamepad1.right_bumper) {
+        lF.setPower(frontLeftPower * 0.75);
+        rF.setPower(frontRightPower * 0.75);
+        lB.setPower(backLeftPower * 0.75);
+        rB.setPower(backRightPower * 0.75);
+      } else if (gamepad1.left_bumper) {
+        lF.setPower(frontLeftPower * 0.25);
+        rF.setPower(frontRightPower * 0.25);
+        lB.setPower(backLeftPower * 0.25);
+        rB.setPower(backRightPower * 0.25);
+      } else {
+        lF.setPower(frontLeftPower);
+        rF.setPower(frontRightPower);
+        lB.setPower(backLeftPower);
+        rB.setPower(backRightPower);
+      }
+
+      // reinitialize field oriented
+      if (gamepad1.left_trigger == 1 && gamepad1.right_trigger == 1) {
+        imu.initialize(parameters);
+      }
+
+      //one button press does one thing
+      wasA = isA;
+      wasX = isX;
+      wasY = isY;
+      wasB = isB;
     }
-
+  }
 }
